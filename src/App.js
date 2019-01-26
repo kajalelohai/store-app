7ae634350af98909327ffbffea8c63c0f5555a88
Kajal Negi <kl.negi03@gmail.com>
2019-01-26 23:40:09 +0530

    feat: add link to the navbar with Link of react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Product from './components/Product';
 import NavBar from './components/Navbar';
 import Details from './components/Details';
 import ProductList from './components/ProductList';
