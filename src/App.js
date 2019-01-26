1cec54bf33ecbc9fc819f93d796b954797b16139
Kajal Negi <kl.negi03@gmail.com>
2019-01-26 21:57:49 +0530

    feat: import all components to app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Product from './components/Product';
+import NavBar from './components/Navbar';
+import Details from './components/Details';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        Hello from app
+        <NavBar></NavBar>
+        <Details></Details>
+        <ProductList></ProductList>
+        <Product></Product>
+        <Cart></Cart>
+        <Default></Default>
       </React.Fragment>
     );
   }
