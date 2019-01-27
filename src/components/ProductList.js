bde0a346382b40c34ea4590393c532b0885a6d8a
Kajal Negi <kl.negi03@gmail.com>
2019-01-27 14:01:15 +0530

    feat: give title component props name and title; import title and pass it props

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
+import { storeProducts } from '../data';
 
 export default class ProductList extends Component {
   state = {
-    products: []
+    products: storeProducts
   };
   render() {
+    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
           <div className="container">
+            <Title name={'Our'} title={'Products'} />
             <div className="row" />
           </div>
         </div>
