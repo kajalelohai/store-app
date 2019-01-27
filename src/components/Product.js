b3c40fb3e859bf6cb7616a9719e0772877c57764
Kajal Negi <kl.negi03@gmail.com>
2019-01-27 17:56:49 +0530

    feat: wip product structure

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,49 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
 
-export default class Product extends Component{
-    render(){
-        return(
-            <div>
-               <h3>Hello from product</h3>
-            </div>
-        )
-    }
+export default class Product extends Component {
+  render() {
+    const { id, title, img, price, incart } = this.props.product;
+    return (
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() => {
+              console.log('You clicked me on the imaage container');
+            }}>
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+            <button
+              className="cart-btn"
+              disabled={incart ? true : false}
+              onClick={() => {
+                console.log('added to the cart');
+              }}>
+              {incart ? (
+                <p className="text-capitalize mb-0" diabled>
+                  {''} in cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
+          </div>
+          {/* Card Footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
+  }
 }
+
+const ProductWrapper = styled.div``;
