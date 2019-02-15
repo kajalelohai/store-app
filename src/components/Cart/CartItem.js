d9c560aa4f3d8a497af16e4093c3ee2809b60a95
kajalelohai <kl.negi03@gmail.com>
2019-02-15 13:19:05 +0530

    feat: add subTotal,tax,total after add to cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -10,14 +10,16 @@ export default function CartItem({ item, value }) {
           src={img}
           style={{ width: "5rem", height: "5rem" }}
           className="img-fluid"
-          alt="item"
+          alt="product"
         />
       </div>
       <div className="col-10 mx-auto col-lg-2">
-        <span className="d-lg-none">product:{title}</span>
+        <span className="d-lg-none">product:</span>
+        {title}
       </div>
       <div className="col-10 mx-auto col-lg-2">
-        <span className="d-lg-none">price:{price}</span>
+        <span className="d-lg-none">price:</span>
+        {price}
       </div>
       <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
         <div className="d-flex justify-content-center">
@@ -48,7 +50,7 @@ export default function CartItem({ item, value }) {
         </div>
       </div>
       <div className="col-10 mx-auto col-lg-2">
-        <strong className="d-lg-none">Item total : $ {total}</strong>
+        <strong>Item total : $ {total}</strong>
       </div>
     </div>
   );
--- src/context.js
+++ src/context.js
@@ -6,7 +6,7 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: storeProducts,
+    cart: [],
     modalOpen: false,
     modelProduct: detailProduct,
     cartSubTotal: 0,
@@ -49,7 +49,7 @@ class ProductProvider extends Component {
         return { products: tempProducts, cart: [...this.state.cart, product] };
       },
       () => {
-        console.log(this.state);
+        this.addTotals();
       }
     );
   };
@@ -81,6 +81,21 @@ class ProductProvider extends Component {
 
   clearCart = id => {
     console.log("this is clear Cart");
+  };
+
+  addTotals = () => {
+    let subTotal = 0;
+    this.state.cart.map(item => (subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(() => {
+      return {
+        cartSubTotal: subTotal,
+        cartTax: tax,
+        cartTotal: total
+      };
+    });
   };
 
   render() {
